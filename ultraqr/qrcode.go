f76eaebc9fab52bb8b46a511f10e29322ec157b9
Petitoto <Petitoto@users.noreply.github.com>
2025-01-14 16:58:20 +0100

    ultraqr: add qrcode generation

--- ultraqr/qrcode.go
+++ ultraqr/qrcode.go
@@ -1,10 +1,19 @@
 package main
 
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/skip2/go-qrcode"
+)
+
 /*
 	Generate a QR code containing the string
 	given as parameter, and returns it as a
 	text-art string.
 */
 func generateQRCode(data string) (string) {
-	
+	qr, err := qrcode.New(data, qrcode.Low)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	return qr.ToSmallString(false)
 }
