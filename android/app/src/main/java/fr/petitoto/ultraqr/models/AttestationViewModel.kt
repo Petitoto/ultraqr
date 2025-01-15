98a269c119090af55a3a52d7966c4f3546f715b4
Petitoto <Petitoto@users.noreply.github.com>
2025-01-15 20:44:43 +0100

    feat(android): create project base and layouts

---
+++ android/app/src/main/java/fr/petitoto/ultraqr/models/AttestationViewModel.kt
@@ -0,0 +1,7 @@
+package fr.petitoto.ultraqr.models
+
+import androidx.lifecycle.ViewModel
+
+class AttestationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ android/app/src/main/java/fr/petitoto/ultraqr/models/DevicesListViewModel.kt
@@ -0,0 +1,7 @@
+package fr.petitoto.ultraqr.models
+
+import androidx.lifecycle.ViewModel
+
+class DevicesListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
